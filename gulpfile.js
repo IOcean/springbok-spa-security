db7d6e17affc6660f58a3df2ebaf5ec5c7ac5d6f
Baptiste Le Bail <blebail@iocean.fr>
2016-02-23 17:42:38 +0100

    build pour module

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use-strict';
+
+const gulp = require('gulp'),
+    del = require('del'),
+    vinylPaths = require('vinyl-paths'),
+    gutil = require('gulp-util'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    eslint = require('gulp-eslint'),
+    Server = require('karma').Server;
+    
+function handleError(err) {
+    gutil.log(err.toString());
+    this.emit('end');
+}
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(vinylPaths(del));
+});
+
+gulp.task('javascript', function() {
+    return gulp.src('app/**/*.js')
+        .pipe(babel())
+        .pipe(concat('springbok-spa-security.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('javascript-min', function() {
+    return gulp.src('app/**/*.js')
+        .pipe(babel())
+        .pipe(concat('springbok-spa-security.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint', function(done) {
+    return gulp.src('app/**/*.js')
+        .pipe(eslint({envs: ['browser', 'es6']}))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('unit', function(done) {
+    new Server({
+            configFile: __dirname + '/karma.conf.js',
+            singleRun: true
+        }, done).start();
+});
+
+gulp.task('default', ['process']);
+
+gulp.task('process', ['clean', 'javascript', 'javascript-min']);
+
+gulp.task('test', ['lint', 'unit']);
---
+++ test/sample.spec.js
@@ -0,0 +1,7 @@
+describe('sample test', function () {
+
+  it('should say Hello to the World', function () {
+    let hello = 'Hello, World!';
+    expect(hello).toEqual('Hello, World!');
+  });
+});
