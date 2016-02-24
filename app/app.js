693ac00ecb75629cb630d303a706121b20125644
Baptiste Le Bail <blebail@iocean.fr>
2016-02-24 10:51:36 +0100

    Service de test

--- app/app.js
+++ app/app.js
@@ -1,4 +1,13 @@
 (function() {
     'use-strict';
     
+    let sec = angular.module('security', []);
+    
+    sec.service('ModuleService', ModuleService);
+    
+    function ModuleService() {
+        this.getValue = function() {
+            return 'value from module';
+        };
+    }
 })();
